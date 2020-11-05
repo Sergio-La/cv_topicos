e6ec08ce6a1e0068647b73e5ad2746831fb59d09
Sergio López <Sergio Lpez@SergioPC>
2020-11-05 16:00:44 -0500

    Version 1.2 taller de netbeans con github

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -17,6 +17,7 @@ public class Cv_ejercicio {
     public static void main(String[] args) {
         System.out.println("versión 0");
         System.out.println("versión 1");
+        System.out.println("sergio lopez");
     }
     
 }
